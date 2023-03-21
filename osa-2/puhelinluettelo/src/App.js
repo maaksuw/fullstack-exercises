3862e6ba11d43e381497f6bd41fac570c699c8ae
Vierimaa Pinja K <vwpinja@cubbli22-gold.local>
2023-03-21 17:11:27 +0200

    Tehtävä 2.6, puhelinluettelo osa 1.

---
+++ osa-2/puhelinluettelo/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addName}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.name}>
+            {person.name}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
