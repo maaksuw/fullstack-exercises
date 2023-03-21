644d497ce07c18dcf04a3d4fb0628e3964e3d079
Vierimaa Pinja K <vwpinja@cubbli22-gold.local>
2023-03-21 20:05:20 +0200

    Tehtävä 2.11, puhelinluettelo osa 6.

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Form = ({form}) => {
   const [addName, handleNameChange, handleNumberChange] = form.functions
@@ -29,15 +30,18 @@ const Persons = ({persons}) => {
 const Filter = ({variable, func}) => <input value={variable} onChange={func} />
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
