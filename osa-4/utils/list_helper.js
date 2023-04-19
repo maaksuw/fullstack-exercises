141d432537b81130104df2d1fda117f54bb9da53
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2023-04-19 20:08:22 +0300

    Tehtävä 4.4 apufunktioita ja yksikkötestejä, osa 2.

--- osa-4/utils/list_helper.js
+++ osa-4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	const sumLikes = (cumul, item) => {
+		return cumul + item.likes
+	}
+	return blogs.reduce(sumLikes, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
