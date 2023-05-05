f2bf4449feb3d934b4c841f38cebf9f022944d3c
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2023-05-05 18:26:43 +0300

    Tehtävä 5.7 blogilistan frontend, osa 7.

--- osa-5/bloglist-frontend/src/components/Blog.js
+++ osa-5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({blog, deleteBlog}) => {
+  const [show, setShow] = useState(false)
+
+  if (show) {
+    return (
+      <div className='blog'>
+        <p>
+          {blog.title} by {blog.author}
+          <Button text="Hide" action={() => setShow(!show)}/>
+        </p>
+        <p>
+          URL: <a href={blog.url}>{blog.url}</a>
+        </p>
+        <p>
+          Likes: {blog.likes}
+          <Button text="Like"/>
+        </p>
+        <p>Added by user: {blog.user.username}</p>
+      </div>
+    )
+  }
+
+  return (
+  <div className='blog'>
+    <p>
+      {blog.title} by {blog.author}
+      <Button text="View" action={() => setShow(!show)}/>
+    </p>
+  </div>
+  )
+}
 
 export default Blog
---
+++ osa-5/bloglist-frontend/src/components/Button.js
@@ -0,0 +1,3 @@
+const Button = ({text, action}) => <button onClick={action} className='button'> {text} </button>
+
+export default Button
